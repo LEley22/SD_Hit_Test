d07e39d769b9d04ea4b95afd7fa5e7388923e078
Lauryn Eley <115225116+LEley22@users.noreply.github.com>
2023-07-26 13:58:55 +0100

    Update senDet.hh

--- senDet.hh
+++ senDet.hh
@@ -1,21 +1,76 @@
-#ifndef SENDET_HH
-#define SENDET_HH
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file B4/B4c/include/CalorimeterSD.hh
+/// \brief Definition of the B4c::CalorimeterSD class
+
+#ifndef senDet_h
+#define senDet_h 1
 
 #include "G4VSensitiveDetector.hh"
-#include "G4AnalysisManager.hh"
-#include "G4RunManager.hh"
+
+#include "Hit.hh"
+
+#include <vector>
 
 class G4Step;
-class G4TouchableHistory;
+class G4HCofThisEvent;
 
-class SenDetClass : public G4VSensitiveDetector
 {
-    public:
-        SenDetClass(G4String name);
-        ~SenDetClass();
-    
-    private:
-        virtual G4bool ProcessHits(G4Step *, G4TouchableHistory *);
+
+/// Calorimeter sensitive detector class
+///
+/// In Initialize(), it creates one hit for each calorimeter layer and one more
+/// hit for accounting the total quantities in all layers.
+///
+/// The values are accounted in hits in ProcessHits() function which is called
+/// by Geant4 kernel at each step.
+
+class senDet : public G4VSensitiveDetector
+{
+  public:
+    senDet(const G4String& name,
+                  const G4String& hitsCollectionName,
+                  G4int nofCells);
+    ~senDet() override = default;
+
+    // methods from base class
+    void   Initialize(G4HCofThisEvent* hitCollection) override;
+    G4bool ProcessHits(G4Step* step, G4TouchableHistory* history) override;
+    void   EndOfEvent(G4HCofThisEvent* hitCollection) override;
+
+  private:
+    HitsCollection* fHitsCollection = nullptr;
+    G4int fNofCells = 0;
 };
 
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
 #endif
+
+
